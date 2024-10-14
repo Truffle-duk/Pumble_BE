ec2f19425e16ab9de63b553bfcdf9f51682292d4
Jayde1012 <ariha1982@naver.com>
2024-10-14 20:00:47 +0900

    [FEAT] 모임장 전용 API 구현

--- src/groupUser/groupUser.service.js
+++ src/groupUser/groupUser.service.js
@@ -1,7 +1,14 @@
-import {changeGroupUserProfile, findImageUrlById, withdrawGroupUser} from "./groupUser.model.js";
+import {
+    changeGroupUserProfile,
+    changeGroupUserRole, deleteGroupModel,
+    findAllGroupUser, findGroupPasswordById,
+    findImageUrlById, updateGroupPasswordModel,
+    withdrawGroupUser
+} from "./groupUser.model.js";
 import {status} from "../../config/responseStatus.js";
 import {BaseError} from "../../config/error.js";
 import {imageDeleter} from "../middleware/s3Manager.js";
+import crypto from "crypto";
 
 export const withdrawGroupService = async (gUserId) => {
     const withdrawGroupResult = await withdrawGroupUser(gUserId)
@@ -40,6 +47,93 @@ export const changeProfileNicknameService = async (gUserId, newName) => {
     const changeProfileNickname = await changeGroupUserProfile('nickname', gUserId, newName)
 
     if (changeProfileNickname.affectedRows === 1) {
+        return {updatedAt: new Date()}
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const driveOutService = async (gUserId) => {
+    const driveOutServiceResult = await changeGroupUserRole('driveOut', gUserId)
+
+    if (driveOutServiceResult && driveOutServiceResult.affectedRows === 1) {
+        return {updatedAt: new Date()}
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const appointService = async (gUserId) => {
+    const appointServiceResult = await changeGroupUserRole('appoint', gUserId)
+
+    if (appointServiceResult && appointServiceResult.affectedRows === 1) {
+        return {updatedAt: new Date()}
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const demotionService = async (gUserId) => {
+    const demotionServiceResult = await changeGroupUserRole('demotion', gUserId)
+
+    if (demotionServiceResult && demotionServiceResult.affectedRows === 1) {
+        return {updatedAt: new Date()}
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const entrustService = async (leaderId, memberId) => {
+    const entrustServiceResult = await changeGroupUserRole('entrust', memberId)
+
+    if (entrustServiceResult && entrustServiceResult.affectedRows === 1) {
+        const changeLeaderToStaffResult = await changeGroupUserRole('appoint', leaderId)
+
+        if (changeLeaderToStaffResult && changeLeaderToStaffResult.affectedRows === 1) {
+            return {updatedAt: new Date()}
+        } else {
+            throw new BaseError(status.DB_ERROR)
+        }
+
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const retrieveAllGroupUserService = async (groupId) => {
+    return await findAllGroupUser(groupId)
+}
+
+export const deleteGroupService = async (groupId, body) => {
+    const realPassword = await findGroupPasswordById(groupId)
+    const inputPassword = crypto.createHash("sha512").update(body.password).digest("base64")
+    if (inputPassword !== realPassword) {
+        throw new BaseError(status.WRONG_PASSWORD)
+    }
+
+    const deleteGroupResult = await deleteGroupModel(groupId)
+    if (deleteGroupResult && deleteGroupResult.affectedRows === 1) {
+        return {deletedAt: new Date()}
+    } else {
+        throw new BaseError(status.DB_ERROR)
+    }
+}
+
+export const retrieveGroupPassword = async (groupId, body) => {
+    const realPassword = await findGroupPasswordById(groupId)
+    const inputPassword = crypto.createHash("sha512").update(body.password).digest("base64")
+    if (inputPassword !== realPassword) {
+        throw new BaseError(status.WRONG_PASSWORD)
+    } else {
+        return "same!"
+    }
+}
+
+export const changeGroupPasswordService = async (groupId, body) => {
+    const newPassword = crypto.createHash("sha512").update(body.newPassword).digest("base64")
+    const changePasswordResult = await updateGroupPasswordModel(groupId, newPassword)
+
+    if (changePasswordResult && changePasswordResult.affectedRows === 1) {
         return {updatedAt: new Date()}
     } else {
         throw new BaseError(status.DB_ERROR)
--- src/groupUser/groupUser.sql.js
+++ src/groupUser/groupUser.sql.js
@@ -2,3 +2,12 @@ export const deleteGroupUser = 'UPDATE GroupUser SET status = ? WHERE group_user
 export const selectImageUrl = 'SELECT profile_image FROM GroupUser WHERE group_user_id = ?;';
 export const updateGroupUserImage = 'UPDATE GroupUser SET profile_image = ? WHERE group_user_id = ?;';
 export const updateGroupUserNickname = 'UPDATE GroupUser SET nickname = ? WHERE group_user_id = ?;';
+
+export const selectAllGroupUser = "SELECT * FROM GroupUser WHERE group_id = ? and status = 'normal';";
+export const updateGroupUserStatus = "UPDATE GroupUser SET status = 'kicked' WHERE group_user_id = ?;";
+export const updateGroupUserRoleToStaff = "UPDATE GroupUser SET role = 'staff' WHERE group_user_id = ?;";
+export const updateGroupUserRoleToMember = "UPDATE GroupUser SET role = 'member' WHERE group_user_id = ?;";
+export const updateGroupUserRoleToLeader = "UPDATE GroupUser SET role = 'leader' WHERE group_user_id = ?;";
+export const selectGroupPassword = "SELECT password FROM `Group` WHERE group_id=?;";
+export const deleteGroup = "DELETE FROM `Group` WHERE group_id = ?;";
+export const updateGroupPassword = "UPDATE `GROUP` SET password = ? WHERE group_id = ?;";

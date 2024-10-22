cefc4865d3530405030d5552c69ac91938244538
Jayde1012 <ariha1982@naver.com>
2024-10-22 23:54:48 +0900

    [REFACTOR] 프로필 조회 응답 수정

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import asyncHandler from "express-async-handler"
 import authChecker from "../middleware/authChecker.js";
-import {getUserNickname, drawout, updateUserNickname} from "./user.controller.js";
+import {drawout, updateUserNickname, getUserNicknameAndEmail} from "./user.controller.js";
 export const userRouter = express.Router()
 
 userRouter.patch("/api/user/nickname", authChecker, asyncHandler(await updateUserNickname))
-userRouter.get("/api/user", authChecker, asyncHandler(await getUserNickname))
+userRouter.get("/api/user", authChecker, asyncHandler(await getUserNicknameAndEmail))
 userRouter.patch("/api/user", authChecker, asyncHandler(await drawout))
